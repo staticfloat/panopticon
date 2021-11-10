60fd9280a8204a79c651429df227a433356deb8d
Elliot Saba <staticfloat@gmail.com>
2021-11-10 23:46:01 +0000

    Grab 120 frames per hour

--- panopticon_encode.py
+++ panopticon_encode.py
@@ -19,7 +19,7 @@ for ip, name in config['cameras'].items():
 
     # Convert the past hour's worth of pictures
     print(f"Encoding last hour of {name}")
-    os.system(f"nice -n 19 {ffmpeg} -y -hide_banner -loglevel error -r 24 -f image2 -start_number {last_hour*120} -i {camdir}/%4d.jpg -frames:v 60 -vf scale=iw/{resolution_divisor}:-1 -c:v libx264 -preset fast -crf 23 {camdir}/hour-{last_hour:02}.mp4")
+    os.system(f"nice -n 19 {ffmpeg} -y -hide_banner -loglevel error -r 24 -f image2 -start_number {last_hour*120} -i {camdir}/%4d.jpg -frames:v 120 -vf scale=iw/{resolution_divisor}:-1 -c:v libx264 -preset fast -crf 23 {camdir}/hour-{last_hour:02}.mp4")
 
     # Concatenate all the hours together to get a "last 24 hours" video
     with tempfile.NamedTemporaryFile(mode='w') as file_list:
