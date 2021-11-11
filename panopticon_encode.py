eb3a62037324f5b50b40ef6deac66f8e8ba58861
Elliot Saba <staticfloat@gmail.com>
2021-11-11 00:23:19 +0000

    Add another print statement

--- panopticon_encode.py
+++ panopticon_encode.py
@@ -22,6 +22,7 @@ for ip, name in config['cameras'].items():
     os.system(f"nice -n 19 {ffmpeg} -y -hide_banner -loglevel error -r 24 -f image2 -start_number {last_hour*120} -i {camdir}/%4d.jpg -frames:v 120 -vf scale=iw/{resolution_divisor}:-1 -c:v libx264 -preset fast -crf 23 {camdir}/hour-{last_hour:02}.mp4")
 
     # Concatenate all the hours together to get a "last 24 hours" video
+    print("Concatenating daily video")
     with tempfile.NamedTemporaryFile(mode='w') as file_list:
         # Generate list of hours that runs chonologically up until the last hour that we just encoded
         hours = [i for i in range(24)]
