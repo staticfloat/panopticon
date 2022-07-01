1fee878ca0d3ef65c8ccdcb278d317fad94421b4
Elliot Saba <staticfloat@gmail.com>
2022-06-30 19:37:31 -0700

    Reintroduce constant spacing
    Now that we use a constant-width font, we can make use of my dad's
    brilliant alignment protocol.

--- panopticon_capture.py
+++ panopticon_capture.py
@@ -138,39 +138,39 @@ def add_weather_text(img_data, weather_str):
 
 def deg_to_direction(deg):
     if deg < 11.25:
-        return "N"
+        return "N  "
     elif deg < 33.75:
         return "NNE"
     elif deg < 56.25:
-        return "NE"
+        return "NE "
     elif deg < 78.75:
         return "ENE"
     elif deg < 101.25:
-        return "E"
+        return "E  "
     elif deg < 123.75:
         return "ESE"
     elif deg < 146.25:
-        return "SE"
+        return "SE "
     elif deg < 168.75:
         return "SSE"
     elif deg < 191.25:
-        return "S"
+        return "S  "
     elif deg < 213.75:
         return "SSW"
     elif deg < 236.25:
-        return "SW"
+        return "SW "
     elif deg < 258.75:
         return "WSW"
     elif deg < 281.25:
-        return "W"
+        return "W  "
     elif deg < 303.75:
         return "WNW"
     elif deg < 326.25:
-        return "NW"
+        return "NW "
     elif deg < 348.75:
         return "NNW"
     else:
-        return "N"
+        return "N  "
 
 
 # If weather info is set up, fetch it and add in!
